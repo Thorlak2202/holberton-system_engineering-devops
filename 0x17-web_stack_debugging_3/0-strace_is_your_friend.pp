251aa6ca8824afbaef2b23c4ed150bf9091a6049
Thorlak2202 <darkdrakkar@gmail.com>
2020-05-20 13:39:13 -0500

    script to fix typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #script to fix typo in configuration settings of wordpress
 
 exec { 'typo_correct':
-  command => "sed -i 's/.phpp/.php' /var/www/html/wp_settings.php",
+  command => "sed -i 's/.phpp/.php' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
