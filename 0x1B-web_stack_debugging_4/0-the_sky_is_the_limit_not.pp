c225aa0e7eb89abd63d9af85a85f5b941d2249b2
Thorlak2202 <darkdrakkar@gmail.com>
2020-05-28 17:51:37 -0500

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 
 exec { 'limit-augment':
   provider => shell,
-  command  => "sed -i 's/15/4096/g' /etc/default/nginx"
+  command  => "sed -i 's/15/4096/g' /etc/default/nginx sudo service nginx restart"
 }
